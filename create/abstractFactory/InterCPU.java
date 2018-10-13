7aad3eb6bd2725ac1a62c9a902aff87e39671e18
houzhenggangGit <hou.zg@foxmail.com>
2018-10-13 16:52:57 +0800

    实现了接口方法

--- create/abstractFactory/InterCPU.java
+++ create/abstractFactory/InterCPU.java
@@ -2,18 +2,35 @@
 * @Description: package abstractFactory; 
 * @author: Arron
 * @date: 2018年10月13日-下午4:37:14
-*/ 
+*/
 
 package abstractFactory;
 
+import sun.management.counter.perf.PerfInstrumentation;
+
 public class InterCPU implements CPU
 {
+	private int pins = 0;
+
+	public int getPins()
+	{
+		return pins;
+	}
+
+	public void setPins(int pins)
+	{
+		this.pins = pins;
+	}
+
+	public InterCPU(int pins)
+	{
+		super();
+		this.pins = pins;
+	}
 
 	@Override
 	public void calulate()
 	{
-		// TODO Auto-generated method stub
-		
+		System.out.println("Intel CPU的针脚数：" + pins);
 	}
-	
 }
