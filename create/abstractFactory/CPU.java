e9a4986808a3d476cd82b06029a2b2c8967b967e
houzhenggangGit <hou.zg@foxmail.com>
2018-10-13 16:42:12 +0800

    init

---
+++ create/abstractFactory/CPU.java
@@ -0,0 +1,12 @@
+/** @version: 1.0
+* @Description: package abstractFactory; 
+* @author: Arron
+* @date: 2018年10月13日-下午4:34:37
+*/ 
+
+package abstractFactory;
+
+public class CPU
+{
+
+}
---
+++ create/abstractFactory/InterCPU.java
@@ -0,0 +1,12 @@
+/** @version: 1.0
+* @Description: package abstractFactory; 
+* @author: Arron
+* @date: 2018年10月13日-下午4:37:14
+*/ 
+
+package abstractFactory;
+
+public class InterCPU
+{
+
+}
